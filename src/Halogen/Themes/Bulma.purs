c4c764b07877b82d3aad4d730e2bef104777f4e1
alebon <alexej.bondarenko@ersocon.net>
2018-09-20 13:22:02 +0200

    Add initial basic classes

---
+++ src/Halogen/Themes/Bulma.purs
@@ -0,0 +1,121 @@
+module Halogen.Themes.Bulma where
+
+import Halogen.HTML.Core (ClassName(..))
+
+
+container :: ClassName
+container = ClassName "container"
+
+content :: ClassName
+content = ClassName "content"
+
+section :: ClassName
+section = ClassName "section"
+
+header :: ClassName
+header = ClassName "header"
+
+footer :: ClassName
+footer = ClassName "footer"
+
+box :: ClassName
+box = ClassName "box"
+
+navBar :: ClassName
+navBar = ClassName "navbar"
+
+navBarMenu :: ClassName
+navBarMenu = ClassName "navbar-menu"
+
+navBarStart :: ClassName
+navBarStart = ClassName "navbar-start"
+
+navBarEnd :: ClassName
+navBarEnd = ClassName "navbar-end"
+
+navBarItem :: ClassName
+navBarItem = ClassName "navbar-item"
+
+navBarLink :: ClassName
+navBarLink = ClassName "navbar-link"
+  
+navBarDropdown :: ClassName
+navBarDropdown = ClassName "navbar-dropdown"
+
+isActive :: ClassName
+isActive = ClassName "is-active"
+
+isHoverable :: ClassName
+isHoverable = ClassName "is-hoverable"
+
+field :: ClassName
+field = ClassName "field"
+
+control :: ClassName
+control = ClassName "control"
+
+hasIconLeft :: ClassName
+hasIconLeft = ClassName "has-icon-left"
+
+hasDropdown :: ClassName
+hasDropdown = ClassName "has-dropdown"
+
+hasShadow :: ClassName
+hasShadow = ClassName "has-shadow"
+
+hasTextCentered :: ClassName
+hasTextCentered = ClassName "has-text-centered"
+
+input :: ClassName
+input = ClassName "input"
+
+isLarge :: ClassName
+isLarge = ClassName "is-large"
+
+isPrimary :: ClassName
+isPrimary = ClassName "is-primary"
+
+isLoading :: ClassName
+isLoading = ClassName "is-loading"
+
+isInfo :: ClassName
+isInfo = ClassName "is-info"
+
+isSuccess :: ClassName
+isSuccess = ClassName "is-success"
+
+isWarning :: ClassName
+isWarning = ClassName "is-warning"
+
+isDanger :: ClassName
+isDanger = ClassName "is-danger"
+
+isMultiple :: ClassName
+isMultiple = ClassName "is-multiple"
+
+table :: ClassName
+table = ClassName "table"
+
+isFullwidth :: ClassName
+isFullwidth = ClassName "is-fullwidth"
+
+button :: ClassName
+button = ClassName "button"
+
+isSmall :: ClassName
+isSmall = ClassName "is-small"
+
+textarea :: ClassName
+textarea = ClassName "textarea"
+
+select :: ClassName
+select = ClassName "select"
+
+isCheckRadio :: ClassName
+isCheckRadio = ClassName "is-checkradio"
+
+tag :: ClassName
+tag = ClassName "tag"
+
+notification :: ClassName
+notification = ClassName "notification"
